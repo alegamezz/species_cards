f3be24b88748fc75616a2b942baec466c4f87c6b
Alejandro Gámez <alejandrogamezct@gmail.com>
2025-11-11 19:56:04 +0100

    print vars

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@
 import { Outlet } from "react-router-dom";
 
 export default function App() {
+  console.log('Supabase URL:', import.meta.env.VITE_SUPABASE_URL);
+  console.log('Supabase Key:', import.meta.env.VITE_SUPABASE_ANON_KEY ? 'Key exists ✅' : '❌ No key');
+
   return (
-    <div className="min-h-screen bg-slate-900 -z-10 text-slate-100">
+    <div className="min-h-screen bg-slate-900 text-slate-100">
       <div>
         <Outlet />
       </div>
